a27d7f3a5d1773ce5e6543c877cc4c7b991f34f4
BI Woo <chooyoonkim@gmail.com>
2026-02-09 02:08:17 +0900

    next.config.ts edited

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // 외부 사진(Supabase 등)을 허용하는 보안 설정입니다.
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // 모든 사이트의 이미지를 허락합니다.
+      },
+    ],
+  },
 };
 
 export default nextConfig;
